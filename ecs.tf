059fee7c17c31c8b71b603664cc77de7068b6347
kasunmaduraeng <kasun@fishpond.co.nz>
2018-11-07 09:46:24 +1300

    new

--- ecs.tf
+++ ecs.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
 module "ecs" {
@@ -22,7 +22,7 @@ module "ecs" {
 
 resource "aws_key_pair" "ecs" {
   key_name   = "ecs-key-${var.environment}"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtMljjj0Ccxux5Mssqraa/iHHxheW+m0Rh17fbd8t365y9EwBn00DN/0PjdU2CK6bjxwy8BNGXWoUXiSDDtGqRupH6e9J012yE5kxhpXnnkIcLGjkAiflDBVV4sXS4b3a2LSXL5Dyb93N2GdnJ03FJM4qDJ8lfDQxb38eYHytZkmxW14xLoyW5Hbyr3SXhdHC2/ecdp5nLNRwRWiW6g9OA6jTQ3LgeOZoM6dK4ltJUQOakKjiHsE+jvmO0hJYQN7+5gYOw0HHsM+zmATvSipAWzoWBWcmBxAbcdW0R0KvCwjylCyRVbRMRbSZ/c4idZbFLZXRb7ZJkqNJuy99+ld41 ecs@aws.fake"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo/oUQw+Bb72uvRv1Vj/OeeMY3euj8fHV+YNJ/JFca/6km+LcKl3tbzgxfn2Gmu2Nyb8wJnrfogh22+PnLANWC2Uk6s8WxXCMa6xIEqom0R51+Bm48ROMrrFwpwcsWw4LifCFZ1pe3XNAy9SwKriVALa/cKW+JGQeSF/sMNL/acsSQXLXAwW8+QMf93v9BByDFOtQl4QwEYx300EkJ/zhp+NTlpT/bjvO4FPH5RHvDz76cxMiPGMVcxM1DKpitBpqtf9tpY4ArMqNhejewF+cjJE3uYlJWqd84IMbdL1/VaGydXt64lKZ14IiVTBVPoNu4lhxkmaYmq4nKLbTdxzjx ec2-user@ip-172-31-27-173e"
 }
 
 variable "vpc_cidr" {}
--- ecs.tfvars
+++ ecs.tfvars
@@ -1,19 +1,19 @@
 vpc_cidr = "10.0.0.0/16"
 
-environment = "acc"
+environment = "dpp"
 
 public_subnet_cidrs = ["10.0.0.0/24", "10.0.1.0/24"]
 
 private_subnet_cidrs = ["10.0.50.0/24", "10.0.51.0/24"]
 
-availability_zones = ["eu-west-1a", "eu-west-1b"]
+availability_zones = ["us-east-1a", "us-east-1b"]
 
-max_size = 1
+max_size = 2
 
-min_size = 1
+min_size = 2
 
-desired_capacity = 1
+desired_capacity = 2
 
 instance_type = "t2.micro"
 
-ecs_aws_ami = "ami-95f8d2f3"
+ecs_aws_ami = "ami-0ac019f4fcb7cb7e6"
